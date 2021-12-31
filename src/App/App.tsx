0f7d52e92b0de9cb6a0fd71b424c207236fddee7
miyamonz <m5813k@gmail.com>
2022-01-01 01:41:42 +0900

    load VRM

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,8 @@
 import { Suspense } from "react";
 import * as THREE from "three";
-import { AppCanvas } from "../AppCanvas";
 import { Octahedron, OrbitControls } from "@react-three/drei";
+import { AppCanvas } from "../AppCanvas";
+import { LoadVRM } from "../LoadVRM";
 
 import { useTurntable } from "./useTurntable";
 function App() {
@@ -17,12 +18,12 @@ function CanvasContent() {
       <axesHelper args={[5]} />
       <gridHelper args={[10, 10]} />
       <directionalLight
-        args={[0xffffff]}
+        args={[0xffffff, 0.2]}
         position={new THREE.Vector3(1, 1, 1).normalize()}
       />
       <OrbitControls />
-      <Suspense fallback={null}>
-        <LoadingSpinner />
+      <Suspense fallback={<LoadingSpinner />}>
+        <LoadVRM url="./models/three-vrm-girl.vrm" />
       </Suspense>
     </>
   );
