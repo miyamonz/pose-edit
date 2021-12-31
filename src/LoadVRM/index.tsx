054d935dbd9fa733fe46af3e027c9192f254b573
miyamonz <m5813k@gmail.com>
2022-01-01 02:13:45 +0900

    fix useVRM

--- src/LoadVRM/index.tsx
+++ src/LoadVRM/index.tsx
@@ -1,19 +1,13 @@
 import { useGLTF } from "@react-three/drei";
 import { VRM } from "@pixiv/three-vrm";
 import usePromise from "react-promise-suspense";
-import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 export function LoadVRM({ url }: { url: string }) {
   const vrm = useVRM(url);
   return <primitive object={vrm.scene} />;
 }
 
-function useVRM(url: string): VRM {
+export function useVRM(url: string): VRM {
   const gltf = useGLTF(url);
-  return usePromise(loadVRM, [gltf]);
-}
-
-async function loadVRM(gltf: GLTF) {
-  const vrm = await VRM.from(gltf);
-  return vrm;
+  return usePromise(VRM.from, [gltf]);
 }
